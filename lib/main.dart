dfd0072f7a8cb28e12fc9352e2c24d07a3d31326
Madushan Perera <madushanperera1212@gmail.com>
2018-11-19 21:50:56 +0530

    Flutter Chat App with Firebase

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_chat/ChatScreen.dart';
+import 'package:flutter_chat/util/Themes.dart';
+
+void main() => runApp(new FlutterChatApp());
+
+class FlutterChatApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Flutter Chat App",
+      theme: defaultTargetPlatform == TargetPlatform.iOS
+          ? Themes.kIOSTheme
+          : Themes.kDefaultTheme,
+      home: new ChatScreen(),
+    );
+  }
+}
